f369f87bd72dc0f02bb5253a4094261f0689de5c
KnightinGale9 <99992991+KnightinGale9@users.noreply.github.com>
2022-11-10 16:42:51 -0800

    Delete src directory

--- src/Observer.java
+++
@@ -1,8 +0,0 @@
-/*
-Observer used in UserFrame as that is where we need to transfer the messages to
- */
-public interface Observer {
-
-    public void update(User user);
-    public String toString();
-}
